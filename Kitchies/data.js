cea35dae87461d8137ba638f16c5d1a1d0f3ee0c
minhlong2301 <tomchanh2301@gmail.com>
2020-11-21 12:15:44 +0700

    data project

--- Kitchies/data.js
+++ Kitchies/data.js
@@ -1,7 +1,141 @@
-// let data = [{
-//     nameFood: 'Chà bông rong biển Đài Loan (taiwan pork floss',
-//     img: '',
-//     content: '',
-//     continents: '',
+let data = [
+    {
+        nameFood: 'Chà bông rong biển Đài Loan (taiwan pork floss)',
+        type: 'Món Á',
+        img: 'Kitchies\img\chabong1.jpg',
+        content: `
+        <p>Chà bông Đài Loan có màu vàng sẫm bắt mắt, tơi và vụn gần như ruốc cá, dễ nhận biết vì khác với màu và hình dáng sợi chà bông Việt. Một số còn có rong biển và vừng đi kèm. Dưới đây mình sẽ giới thiệu cách làm chà bông của người Hoa (pinyin gọi là rou song), gia vị các bạn có thể biến tấu theo sở thích (có người thích mặn ngọt, có người thích cay…), còn công thức này là công thức phổ biến nhất vì gia vị rất cơ bản và mình thấy khá nhiều người sử dụng.</p>
+        <p>Trong nhiều công thức người ta dùng máy làm bánh mì để làm chà bông, vì mình (và rất nhiều bạn) không có máy này nên mình sử dụng 4 phương pháp đơn giản và ra thành phẩm bông và tơi không kém, đó là sử dụng máy đánh trứng, máy xay thịt, máy xay sinh tố và chà tay.</p>
+        <ul>
+            <li>Với máy xay thịt: mình dùng Bosch 800w có lưỡi dao nhựa, xay tới khi đủ tơi và sợi nhỏ thì dừng, nếu không có lưỡi nhựa các bạn xài lưỡi sắt nhưng dán băng keo lại hoặc sử dụng lưỡi sắt nhưng nhấp nhả 1-2 giây để chà bông không vụn như bột.</li>
+            <img src="Kitchies\img\chabong2.jpg" alt="">
+            <li>Dùng máy sinh tố: cũng xay tới khi bạn thấy đủ bông, tơi, sợi nhỏ vụn (nhưng không phải bột).</li>
+            <li>Máy đánh trứng cầm tay: dùng que xoắn hoặc que phới lồng đánh trứng đều được. Mình dùng thử que xoắn thì thành phẩm khá ổn, bông, tơi nhưng không đều như máy xay thịt và hơi mỏi tay xíu.</li>
+            <li>Chà tay: vừa rang trên chảo vừa cầm thịt chà vào nhau, thao tác gần giống như vò quần áo, hoặc dùng má bàn tay áp vào nhau để chà. Trong ảnh mình mới chà sơ sơ cũng thấy thịt bắt</li>
+            <strong>NGUYÊN LIỆU:</strong>
+            <li>1 kg thịt lợn (thịt đùi, thịt mông hoặc thịt thăn, mình dùng thịt thăn), có thể thay bằng ức gà để làm chà bông gà</li>
+            <li>1 mẩu quế bé cỡ ngón tay cái</li>
+            <li>2 bông hồi</li>
+            <li>5-6 lát gừng</li>
+            <li>2-3 cây hành lá</li>
+            <li>4 lá nguyệt quế</li>
+            <li>2 thìa ăn rượu Thiệu Hưng (hoặc rượu nếp)</li>
+            <li>1 thìa cafe tiêu Xuyên (tùy chọn)</li>
+            <li>2 tấm rong biển khô loại dùng để cuốn sushi (Nori) (tùy chọn – giúp chà bông thơm hơn)</li>
+            <li>vừng rang (tùy chọn – số lượng tùy thích)</li>
+            <p>Hỗn hợp ướp thịt:</p>
+            <li>3 thìa ăn xì dầu (mình dùng Lee Kum Kee)</li>
+            <li>2 thìa ăn dầu ăn</li>
+            <li>1 thìa ăn dầu hào</li>
+            <li>1 thìa cafe muối</li>
+            <li>2-3 thìa cafe đường</li>
+            <li>1/2 thìa cafe ngũ vị hương (tùy chọn)</li>
+            <li>1,5-2 thìa ăn hắc xì dầu (mình dùng Lee Kum Kee)-để tạo màu như hình</li>
+            <img src="Kitchies\img\chabong5.jpg" alt="">
+            <strong>CÁCH LÀM:</strong>
+            <p>1.Thịt lợn các bạn lược bỏ phần mỡ bám, sau đó thái thành miếng hộp diêm. Tiến hành đun sôi rồi hớt bọt, hạ lửa và cho vào nồi quế, hồi, thảo quả, rượu, hành và gừng, đậy vung nấu khoảng 40 phút tới khi chọc đũa với lực không quá mạnh thấy đâm được thịt thì tắt bếp, cho ra rổ rửa sạch với nước lạnh rồi để ráo. Thịt không nên dai quá, khó xé và khó bông. Thịt mềm vừa chín tới, không quá nhũn mềm như thịt đông là được.</p>
+            <p>2.Cho thịt vào túi ziplock dùng chày cối hoặc vật nặng đập nhẹ cho thịt tơi ra (không dằn mạnh sẽ nát thịt).</p>
+            <p>3.Dùng tay xé sợi nhỏ. Rồi ướp thịt cùng gia vị, bóp đều rồi nêm nếm cho hợp khẩu vị của các bạn nhé.</p>
+            <p>4.Cho thịt lên chảo, rang lửa nhỏ vừa tới khi thịt có độ dai, chắc, hơi khô phía ngoài. Trong quá trình đảo nếu thấy sợi nào còn to thì tranh thủ xé nhỏ tiếp. Lâu lâu dùng má bàn tay vò vò qua cho thịt tơi ra xíu.</p>
+            <p>5.Cho thịt vào máy xay (vui lòng đọc phía đầu bài hướng dẫn cách đánh bông thịt). Thịt tơi thì đổ ra âu. Trộn rong biển cắt vụn vào và trộn đều rồi cho lên chảo đảo tiếp.</p>
+            <p>6.Thịt càng tơi thì càng nhẹ bông, nhanh khô nên sau khi đánh bông chỉ cần cho lên chảo đảo thêm 2 phút, sau đó rắc vừng rang lên trộn và trải ra khay phơi cho nguội. </p>
+            <p>7.Thành phẩm có thể ăn cùng cơm, cháo, ăn vã, kẹp bánh mì, và đặc biệt là để làm bánh mì mặn chà bông, bánh bông lan cuộn rắc chà bông…(đây là mục đích chính của mình nè kkk)</p>
+            <p>8.1kg thịt mình làm được khoảng loanh quanh 400gr gì đó, có thể ít hơn hoặc nhiều hơn vài chục gram. Làm kỹ thì để nhiều tháng không hỏng, bảo quản tủ mát, tuy nhiên cố gắng ăn nhanh nhanh vì để lâu nó ám mùi khác vào mất vị thơm ngon của chà bông. </p>
+            </ul>
 
-// }]
+            <p>Chúc các bạn thành công với món chà bông nhé! Và cuối tuần vui vẻ nữa :X</p>`,
+
+    }
+
+    {
+        nameFood:'Thịt kho Tô Đông Pha - Braised pork belly Dong Po Rou',
+        type:'Món Á',
+        img:'Kitchies\img\thitkho1.jpg',
+        content:`
+        <p>Thịt kho Đông Pha (东坡肉) là một món ăn truyền thống của Hàng Châu, Trung Quốc được làm từ thịt ba chỉ, được đặt tên theo ông Tô Thức hay còn có hiệu là Tô Đông Pha (08/01/1037-24/08/1101). Ông là nhà văn, nhà thơ, họa sỹ, nhà thư pháp nổi tiếng Trung Quốc vào thời Tống. Ông được mệnh danh là một trong Bát đại gia Đường Tống (nguồn: Wikipedia). Trải qua nhiều thế kỷ, món thịt kho Đông Pha trở nên nổi tiếng khắp nơi vì hương vị thơm ngon, mềm mại và béo ngậy của nó.</p>
+        <p>Bí quyết để nấu món thịt kho Đông Pha đó là rượu Thiệu Hưng và thời gian nấu. Rượu Thiệu Hưng kết hợp với hành gừng tỏi cho món thịt có mùi thơm đặc trưng, thời gian nấu đủ lâu để miếng thịt tan chảy ngay khi cho vào miệng. Và thịt để kho phải là thịt ba chỉ.</p>
+        <img src="Kitchies\img\thitkho2.jpg" alt="">
+        <strong>NGUYÊN LIỆU:</strong>
+        <ul>
+            <li>600gr thịt ba chỉ, chọn khúc dày, mỡ nạc đều nhau</li>
+            <li>6 lát gừng cỡ đồng xu</li>
+            <li>1-2 bó hành lá (tùy độ dày mỏng)</li>
+            <li>1/4 cup rượu Thiệu Hưng (khoảng 125ml)</li>
+            <li>3,5-4 thìa ăn xì dầu (mình dùng hiệu Lee Kum Kee)</li>
+            <li>1,5 thìa ăn hắc xì dầu (mình dùng hiệu Lee Kum Kee)</li>
+            <li>50gr đường phèn (khoảng 2-2,5 thìa ăn)</li>
+            <li>khoảng 250ml nước</li>
+            <p>Phần trang trí (không bắt buộc – nhưng nếu muốn màu mè như ảnh thì làm theo)</p>
+            <li>2 cây hành lá (chỉ lấy phần lá xanh)</li>
+            <li>1 thìa ăn tinh bột ngô</li>
+            <li>50ml nước</li>
+            <img src="Kitchies\img\thitkho3.jpg" alt="">
+            <strong>CÁCH LÀM:</strong>
+            <p>1.Thịt ba chỉ rửa sạch. Nếu còn có lông bám thì chà lớp da vào chảo nóng cho sạch. Sau đó luộc qua thịt ba chỉ để loại bỏ bẩn và sau đó định hình miếng thịt tốt hơn vì nếu cắt khúc lúc thịt còn tươi sống thì khi nấu nó co lại mất đi hình dạng vuông vắn ban đầu.</p>
+            <p>2.Sau khi luộc sơ thì rửa miếng thịt, cắt thịt thành từng miếng vuông khoảng 4-5cm. Dùng dây chỉ nấu ăn buộc lại để giúp nạc và mỡ không bị tách nhau ra trong quá trình ninh nhừ (cái này không bắt buộc)</p>
+            <p>3.Dùng nồi đế dày (nồi đất nồi đá…) rồi xếp hành lá và gừng lát xuống đáy nồi ngay ngắn bằng phẳng rồi xếp thịt vào, phần da ở dưới nhé. Cho các gia vị ở trên vào nồi thịt. Các bạn nên chọn nồi có kích cỡ phù hợp với lượng thịt để khi đổ nước vào đủ xâm xấp mặt thịt là tốt nhất. (như nồi của mình thì khoảng 250ml là đầy). Đun sôi rồi hạ lửa xuống nhỏ vừa khoảng 1 tiếng rưỡi. Không cần lật mặt thịt, có thể hớt bọt nếu có.</p>
+            <p>4.Sau 1 tiếng rưỡi kho thịt các bạn có 2 lựa chọn: hoặc lật mặt thịt lên và đun tiếp 1 tiếng rưỡi. Hoặc chọn cách hấp. Mình chọn cách hấp thịt 😀 Cả 2 cách đều cho bạn miếng thịt mềm và thơm. Lúc hấp có thể gỡ dây ra để thịt không bị in hình dây buộc nữa</p>
+            <p>5.Các bạn cho nguyên nồi thịt vào nồi hấp, phần da ở trên; hoặc cho thịt ra bát con rồi cho chút nước vào thịt, phần da ở trên rồi hấp. Thời gian hấp là 1 tiếng rưỡi. Sau đó lấy ra đĩa, ăn cùng cơm nóng.</p>
+            <p>6.Bước này không bắt buộc, dành cho các bạn thích sống ảo như mình 😀 hành lá các bạn tước nhỏ rồi ngâm vào nước thật lạnh tầm 30 phút cho hành quăn lại thì trang trí sẽ đẹp hơn. 50ml nước hòa cùng một thìa ăn bột năng rồi cho vào nồi nước kho thịt còn dư và đun cho nước thịt quánh lại chút. Sau đó xếp thịt lên đĩa, rưới nước thịt lên thì các bạn sẽ có lớp nước thịt quánh sệt, tạo nên độ bóng cho miếng thịt, nhất là phần da.</p>
+        </ul>`,
+    }
+
+    {
+        nameFood:'Cháo ếch Singapore - Forg leg porrideg',
+        type:'Món Á',
+        img:'Kitchies\img\chaoech1.jpg',
+        content:`
+        <p>Mình mua được bịch đùi ếch đông lạnh nên nảy sinh ra ý tưởng là sẽ nấu món cháo ếch Singapore. Tiếc là cái đùi hơi bé nên không được dày thịt như ếch ở quê nhà, lại chỉ có mỗi cái đùi. Mình đem nấu cháo một nửa còn một nửa để dành bữa nào làm ếch chiên xù.</p>
+        <p>Thành phẩm có vị mặn đậm đà, hơi ngọt, cay cay, thơm mùi gừng phi, dầu mè và rượu, chan vào cùng với cháo ăn rất vừa miệng.</p>
+        <img src="Kitchies\img\chaoech2.jpg" alt="">
+        <strong>NGUYÊN LIỆU:</strong>
+        <ul>
+            <li>500-600gr ếch đã lột vỏ, làm sạch</li>
+            <li>6-7 lát gừng cỡ đồng xu</li>
+            <li>2 cây hành lá, thân trắng để riêng</li>
+            <li>khoảng 3-5 quả ớt khô</li>
+            <li>1 quả ớt cay tươi (bỏ qua nếu ăn ít cay)</li>
+            <li>1 thìa dầu ăn</li>
+            <li>1 thìa ăn (muỗng gạt) tinh bột ngô (hoặc bột năng) hòa cùng 50ml nước lọc</li>
+
+            <strong>Sốt ướp:</strong>
+            <li>1 thìa ăn dầu hào</li>
+            <li>1 thìa ăn tinh bột ngô</li>
+            <li>1 thìa cafe dầu mè</li>
+            <li>1 thìa ăn hắc xì dầu</li>
+            <li>1 thìa ăn rượu Thiệu Hưng (có thể thay tạm bằng rượu trắng)</li>
+            <li>1/2 thìa cafe muối nở baking soda (tùy chọn – giúp thịt mềm)</li>
+            <li>1/4 thìa cafe tiêu bột trắng (hoặc tiêu đen)</li>
+            <li>2 thìa ăn xì dầu (soy sauce)</li>
+
+            <strong>Sốt nấu:</strong>
+            <li>1,5 thìa ăn xì dầu (soy sauce)</li>
+            <li>1 thìa ăn rượu Thiệu Hưng (có thể thay tạm bằng rượu trắng)</li>
+            <li>1 thìa ăn dầu hào</li>
+            <li>1 thìa cafe hắc xì dầu</li>
+            <li>1,5 thìa ăn đường</li>
+            <li>1/4 thìa cafe muối</li>
+            <li>1/4 thìa cafe tiêu bột trắng (hoặc tiêu đen)</li>
+            <li>150ml nước dùng gà (hoặc nước lọc)</li>
+            
+            <p>Nấu cháo ăn kèm:</p>
+
+
+            <li>Gạo nếp và gạo tẻ tỉ lệ 1 nếp : 4 tẻ (hoặc cơm nguội – chọn gạo dẻo thì nấu cháo sẽ ngon hơn)</li>
+            <li>nước luộc gà (hoặc nước lọc pha cùng hạt nêm, bột gà…)</li>
+            <li>tiêu bột trắng để rắc lên cháo</li>
+            <li>ít hành lá thái nhỏ để rắc lên cháo</li>
+            <img src="Kitchies\img\chaoech3.jpg" alt="">
+
+            <strong>CÁCH LÀM:</strong>
+
+            <p>1.Ớt khô ngâm nước ấm tầm 15-20 phút cho mềm, sau đó cắt đôi cắt 3, nếu sợ cay các bạn nhớ bỏ hạt đi. Cho ớt khô giúp món ăn có hương vị ớt dậy mùi hơn. Phần thân trắng của hành lá thái nhỏ, phần hành lá xắt cọng dài cỡ 4-5cm. Hỗn hợp sốt nấu pha sẵn để riêng.</p>
+            <p>2.Gạo và nếp rang lên cho trắng đục, sau đó cho nước vào nấu. Cháo không nên đặc quá cũng không nên loãng quá. Hoặc dùng trực tiếp cơm để nấu sẽ rút bớt được khá nhiều thời gian. Dùng tay đánh hoặc dùng máy xay cầm tay xay qua một chút cho cháo nhuyễn hơn (mình dùng máy xay sinh tố cầm tay). Cháo sẽ thơm ngon hơn khi nấu cùng nước luộc gà, nếu không có thì cho 1 chút bột nêm gà hoặc hạt nêm vào.</p>
+            <p>3.Ếch rửa sạch, dùng rượu để khử tanh, bóc hết màng đen bẩn. Chặt nhỏ miếng vừa ăn. Sau đó ướp với hỗn hợp sốt ướp khoảng 30 phút – 1 tiếng.</p>
+            <p>4.Cho 1 thìa dầu ăn vào nồi, phi thơm gừng và thân trắng của hành lá, sau đó cho ớt tươi và ớt khô vào phi thơm. Cho ếch vào xào sơ tầm 1-2 phút rồi cho hỗn hợp sốt nấu vào, đun sôi rồi tới khi ếch vừa chín tới thì cho hỗn hợp tinh bột ngô đã hòa với nước vào cho nước sánh lại thì tắt bếp. Thả hành lá đã thái khúc vào đảo qua. Không đun quá lâu thì thịt ếch dễ bị nát.</p>
+            <p>5.Múc ếch cùng nước sốt ra ăn cùng với cháo trắng. Rắc chút hành lá và tiêu nếu thích.</p>
+        </ul>`,
+        
+    }
+]
+
