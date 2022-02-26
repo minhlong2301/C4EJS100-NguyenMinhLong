5001187259571caeefd04007f318628b59b47429
minhlong2301 <72876823+minhlong2301@users.noreply.github.com>
2022-02-26 20:41:14 +0700

    Delete test directory

--- test/a-1/index.js
+++
@@ -1,7 +0,0 @@
-function findOppositeNumber(n, firstNum) {
-    const step = n / 2;
-    return firstNum >= step ? firstNum - step : firstNum + step;
-}
-
-console.log(findOppositeNumber(10, 2));
-console.log(findOppositeNumber(10, 6));
