64a72826e5a72ae3508f6772ea72b408f535ab8b
minhlong2301 <72876823+minhlong2301@users.noreply.github.com>
2022-02-26 20:41:07 +0700

    Delete session9 directory

--- session9/homework6/index.js
+++
@@ -1,9 +0,0 @@
-function distanceBetween2points(x1, y1, x2, y2){
-    return Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
-}
-const d = distanceBetween2points(3, 10, 0 ,6);
-if (d !== 5) {
-    console.log('Failed: the calculation is wrong');
-} else {
-    console.log('Passed, bravo');
-}
